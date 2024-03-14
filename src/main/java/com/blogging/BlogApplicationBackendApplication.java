3945541252ee305919b606dd743a88799bb19d7c
Mihir Dadhich <mihir2029dadhich@gmail.com>
2024-03-14 14:31:31 +0530

    Your commit message here

--- src/main/java/com/blogging/BlogApplicationBackendApplication.java
+++ src/main/java/com/blogging/BlogApplicationBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.blogging")
+@EnableJpaRepositories(basePackages = "com.blogging.entities")
 public class BlogApplicationBackendApplication {
 
 	public static void main(String[] args) {
